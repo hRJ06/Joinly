57c9fe1bb50392764f510f8c0fa2750033f8a79a
hRJ06 <hindolroy.2306@gmail.com>
2024-03-08 21:01:08 +0530

    Added Initial Commit for Joinly

---
+++ provider/Convex-Client-Provider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Loading } from "@/components/auth/loading";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexURL = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexURL);
+
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
