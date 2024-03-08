d347881ca7a99fd163cb7be4647c77977e1de056
hRJ06 <hindolroy.2306@gmail.com>
2024-03-08 21:21:57 +0530

    Added Dashboard Layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,25 @@
+import { NavBar } from "./_component/Navbar";
+import { OrgSidebar } from "./_component/Org-Sidebar";
+import { Sidebar } from "./_component/Sidebar";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <main className="h-full">
+      <Sidebar />
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <NavBar />
+            {children}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default DashboardLayout;
