8edcc8ef8d624339deb33637421181e5612ef63f
hRJ06 <hindolroy.2306@gmail.com>
2024-03-09 00:19:51 +0530

    Added Sidebar

--- app/(dashboard)/_component/Sidebar/index.tsx
+++ app/(dashboard)/_component/Sidebar/index.tsx
@@ -1,7 +1,11 @@
+import { List } from "./List";
+import { NewButton } from "./NewButton";
+
 export const Sidebar = () => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4">
-      
+      <List />
+      <NewButton />
     </aside>
   );
 };
