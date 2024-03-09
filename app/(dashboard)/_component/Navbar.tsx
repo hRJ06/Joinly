24347b558b9e3ed1207dd459a39e744fe9e48ecf
hRJ06 <hindolroy.2306@gmail.com>
2024-03-09 11:07:07 +0530

    Added Organization Switcher support in NavBar

--- app/(dashboard)/_component/Navbar.tsx
+++ app/(dashboard)/_component/Navbar.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export const NavBar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+    <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">{/* TODO: ADD SEARCH */}</div>
       <UserButton />
     </div>
