cb88df76d07457353a3d605cad60395c80d74069
hRJ06 <hindolroy.2306@gmail.com>
2024-03-10 00:02:54 +0530

    Added More Functionality for Board Card

--- app/(dashboard)/_component/NewBoardButton.tsx
+++ app/(dashboard)/_component/NewBoardButton.tsx
@@ -1,29 +1,37 @@
 "use client";
 
 import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import { cn } from "@/lib/utils";
-import { useMutation } from "convex/react";
 import { Plus } from "lucide-react";
+import { toast } from "sonner";
 
 interface NewBoardButtonProps {
   orgId: string;
   disabled?: boolean;
 }
 export const NewBoardButton = ({ orgId, disabled }: NewBoardButtonProps) => {
-  const create = useMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
   const onClick = () => {
-    create({
+    mutate({
       orgId,
       title: "Untitled",
-    });
+    })
+      .then((id) => {
+        toast.success("Board Created");
+      })
+      .catch((err) => {
+        toast.error("Failed to create board");
+      });
   };
   return (
     <button
-      disabled={disabled}
+      disabled={pending || disabled}
       onClick={onClick}
       className={cn(
         "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
-        disabled && "opacity-75"
+        (pending || disabled) &&
+          "opacity-75 hover:bg-blue-600 cursor-not-allowed"
       )}
     >
       <div />
---
+++ components/actions.tsx
@@ -0,0 +1,77 @@
+"use client";
+/* https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_event_stoppropagation */
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import Link from "next/link";
+import { Link2, Trash2 } from "lucide-react";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+import { toast } from "sonner";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { ConfirmModal } from "./ConfirmModal";
+import { Button } from "./ui/button";
+
+interface ActionsProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionsProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link Copied"))
+      .catch(() => toast.error("Failed To Copy Link"));
+  };
+  const onDelete = () => {
+    mutate({
+      id,
+    })
+      .then(() => toast.success("Board Deleted"))
+      .catch(() => toast.error("Failed To Delete Board"));
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => e.stopPropagation()}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem className="p-3 cursor-pointer" onClick={onCopyLink}>
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy Board Link
+        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delete Board?"
+          description="This will delete Board and all of its contents."
+          disbabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal"
+            variant="ghost"
+          >
+            <Trash2 className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
