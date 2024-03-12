fa46d3e82d9e58581a08c9c7f3555e092bf756cc
hRJ06 <hindolroy.2306@gmail.com>
2024-03-12 15:51:28 +0530

    Added Canvas Setup

---
+++ app/folder/[boardId]/_components/Canvas.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Info } from "./Info";
+import { Participants } from "./Participants";
+import { Toolbar } from "./Toolbar";
+
+interface CanvasProps {
+  boardId: string;
+}
+export const Canvas = ({ boardId }: CanvasProps) => {
+  return (
+    <main className="h-full w-full relative bg-neutral-100 touch-none">
+      <Info />
+      <Participants />
+      <Toolbar />
+    </main>
+  );
+};
