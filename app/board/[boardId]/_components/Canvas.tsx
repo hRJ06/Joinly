9c43dc5dd2ba4a33862c0390b211107e36edc009
hRJ06 <hindolroy.2306@gmail.com>
2024-03-21 08:35:48 +0530

    Added Dependency, Folder Structure and Config

--- app/board/[boardId]/_components/Canvas.tsx
+++ app/board/[boardId]/_components/Canvas.tsx
@@ -1,21 +1,43 @@
 "use client";
 
+import { useState } from "react";
 import { Info } from "./Info";
 import { Participants } from "./Participants";
 import { Toolbar } from "./Toolbar";
-import { useSelf } from "@/liveblocks.config";
+import {
+  useCanRedo,
+  useCanUndo,
+  useHistory,
+  useSelf,
+} from "@/liveblocks.config";
+import { CanvasMode, CanvasState } from "@/types/Canvas";
 
 interface CanvasProps {
   boardId: string;
 }
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   const info = useSelf((me) => me.info);
   console.log(info);
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
--- app/board/[boardId]/_components/Toolbar.tsx
+++ app/board/[boardId]/_components/Toolbar.tsx
@@ -1,71 +1,84 @@
-import { Circle, MousePointer2, Pencil, Redo2, Square, StickyNote, Type, Undo2 } from "lucide-react";
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react";
+import { CanvasMode, CanvasState } from "@/types/Canvas";
 import { ToolButton } from "./ToolButton";
 
-export const Toolbar = () => {
+interface ToolbarProps {
+  canvasState: CanvasState;
+  setCanvasState: (newState: CanvasState) => void;
+  undo: () => void;
+  redo: () => void;
+  canUndo: boolean;
+  canRedo: boolean;
+}
+export const Toolbar = ({
+  canvasState,
+  setCanvasState,
+  undo,
+  redo,
+  canUndo,
+  canRedo,
+}: ToolbarProps) => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-      <ToolButton
+        <ToolButton
           label="Select"
           icon={MousePointer2}
-          onClick={() => {}}
-          isActive={
-            false
-          }
+          onClick={() => setCanvasState({ mode: CanvasMode.None })}
+          isActive={canvasState.mode === CanvasMode.None}
         />
         <ToolButton
           label="Text"
           icon={Type}
           onClick={() => {}}
-          isActive={
-            false
-          }
+          isActive={false}
         />
         <ToolButton
           label="Sticky Note"
           icon={StickyNote}
           onClick={() => {}}
-          isActive={
-            false
-          }
+          isActive={false}
         />
         <ToolButton
           label="Rectangle"
           icon={Square}
           onClick={() => {}}
-          isActive={
-            false
-          }
+          isActive={false}
         />
         <ToolButton
           label="Ellipse"
           icon={Circle}
           onClick={() => {}}
-          isActive={
-            false
-          }
+          isActive={false}
         />
         <ToolButton
           label="Pen"
           icon={Pencil}
           onClick={() => {}}
-          isActive={
-            false
-          }
+          isActive={false}
         />
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-      <ToolButton
+        <ToolButton
           label="Undo"
           icon={Undo2}
-          onClick={() => {}}
-          isDisabled={true}
+          onClick={undo}
+          isDisabled={!canUndo}
         />
         <ToolButton
           label="Redo"
           icon={Redo2}
-          onClick={() => {}}
-          isDisabled={true}
+          onClick={redo}
+          isDisabled={!canRedo}
         />
       </div>
     </div>
