2ce1f8bb7e38b8a0ea88bd70648ef38b592d82fe
hRJ06 <hindolroy.2306@gmail.com>
2024-04-24 18:21:38 +0530

    Added Text

--- app/board/[boardId]/_components/LayerPreview.tsx
+++ app/board/[boardId]/_components/LayerPreview.tsx
@@ -3,6 +3,8 @@ import { useStorage } from "@/liveblocks.config";
 import { LayerType } from "@/types/Canvas";
 import { memo } from "react";
 import { Rectangle } from "./Rectangle";
+import { Ellipse } from "./Ellipse";
+import { Text } from "./Text";
 interface LayerPreviewProps {
   id: string;
   onLayerPointerDown: (e: React.PointerEvent, layerId: string) => void;
@@ -14,7 +16,32 @@ export const LayerPreview = memo(
     if (!layer) return null;
     switch (layer.type) {
       case LayerType.Rectangle:
-        return <Rectangle id={id} layer={layer} onPointerDown={onLayerPointerDown} selectionColor={selectionColor}/>;
+        return (
+          <Rectangle
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Ellipse:
+        return (
+          <Ellipse
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      case LayerType.Text:
+        return (
+          <Text
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
       default:
         console.warn("Unknown Layer Type");
         return null;
